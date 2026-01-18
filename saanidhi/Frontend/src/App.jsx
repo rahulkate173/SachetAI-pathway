f838140e79bb5f5d4fe5715dd17fb342e751020a
rahulkate173 <rahulkate173@gmail.com>
2026-01-18 13:15:02 +0530

    Stop tracking personal folders

--- saanidhi/Frontend/src/App.jsx
+++
@@ -1,5 +0,0 @@
-import MainLayout from "./layout/MainLayout.jsx";
-
-export default function App() {
-  return <MainLayout />;
-}
--- saanidhi/Frontend/src/hooks/useSidebar.js
+++
@@ -1,6 +0,0 @@
-import { useState } from "react";
-
-export function useSidebar() {
-  const [collapsed, setCollapsed] = useState(false);
-  return { collapsed, setCollapsed };
-}
